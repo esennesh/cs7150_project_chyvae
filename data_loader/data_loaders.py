5b0f4506733d1f6be2fc164f9aabc5074dfbfbcd
esennesh <elisennesh@gmail.com>
2020-07-30 16:21:27 -0400

    data_loaders: write dataset object and data-loader for dSprites
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,53 @@
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
+import numpy as np
+import os
+from PIL import Image
+import wget
+
 from base import BaseDataLoader
 
+DSPRITES_URL = 'https://github.com/deepmind/dsprites-dataset/raw/master/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz'
+DSPRITE_FILENAME = 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz'
 
-class MnistDataLoader(BaseDataLoader):
+class dSprites(Dataset):
+    def __init__(self, root='./', download=True, transform=None):
+        self.root = root
+        self.transform = transform
+
+        if not os.path.exists(self.root + DSPRITE_FILENAME) and download:
+            wget.download(DSPRITES_URL, self.root + DSPRITE_FILENAME)
+
+        # Load dataset
+        dataset_zip = np.load(self.root + DSPRITE_FILENAME)
+        print('Keys in the dataset:', dataset_zip.keys())
+        self.imgs = dataset_zip['imgs']
+        self.latents_values = dataset_zip['latents_values']
+        self.latents_classes = dataset_zip['latents_classes']
+        #self.metadata = dataset_zip['metadata'][()]
+        #print('Metadata: \n', metadata)
+        print('Dataset loaded : OK.')
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        image = Image.fromarray(self.imgs[idx])
+        latent = self.latents_values[idx]
+
+        if self.transform is not None:
+            image = self.transform(image)
+            return (image, latent)
+
+class dSpriteDataLoader(BaseDataLoader):
     """
-    MNIST data loading demo using BaseDataLoader
+    dSprites data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.1, num_workers=1):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+            # transforms.Normalize((0.1307,), (0.3081,))
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = dSprites(self.data_dir, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
