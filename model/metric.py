d179cce524a8e5ffe431df7479fabcc97e0336ca
esennesh <elisennesh@gmail.com>
2020-08-03 00:58:59 -0400

    model.metric: add the disentanglement metric
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- model/metric.py
+++ model/metric.py
@@ -21,3 +21,31 @@ def reconstruction_error(output, target, metadata=None):
     observation = p['reconstruction'].value
     cross_entropy = base.probtorch_cross_entropy(reconstruction, observation)
     return cross_entropy.mean(dim=0)
+
+def _sample_var_batch(metadata, scale, B=20):
+    bX, by = [], []
+    for _ in range(B):
+        xi, factor = metadata['dataset'].sample_fixed_factor(size=200)
+        xi = torch.Tensor(xi).to(metadata['device']).transpose(-3, -1)
+        p, _, _ = metadata['model'].forward(xi)
+        zi = p['z'].dist.loc / scale
+        D = torch.var(zi, dim=0).argmin().item()
+        f = factor - metadata['dataset'].diff
+        bX.append(D)
+        by.append(f)
+    return torch.tensor(bX), torch.tensor(by)
+
+def disentanglement_metric(output, target, metadata):
+    p, q, _ = output
+
+    scale = p['z'].value.std(dim=0)
+    batch_imgs, batch_classes = _sample_var_batch(metadata, scale, B=800)
+    V = torch.zeros(metadata['model'].z_dim, metadata['dataset'].n_factors)
+    for imgs, targets in zip(batch_imgs, batch_classes):
+        V[imgs, targets] += 1
+
+    target_imgs, target_classes = _sample_var_batch(metadata, scale, B=800)
+    V_targets = torch.stack([V[target] for target in target_imgs], dim=0)
+    preds = torch.argmax(V_targets, dim=-1)
+
+    return torch.mean((preds == target_classes).to(dtype=torch.float), dim=0)
